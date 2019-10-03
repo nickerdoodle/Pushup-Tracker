a99b12a7946478b44ee2377eb0c4d4d247d2d914
Nick <nicholasmahe@gmail.com>
2019-10-02 18:14:09 -0600

    First commit

---
+++ Project 1 Mahe Nick/Controllers/LogsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LogsViewController.swift
+//  Project 1 Mahe Nick
+//
+//  Created by Nick Mahe on 10/2/19.
+//  Copyright © 2019 Nick Mahe. All rights reserved.
+//
+
+import UIKit
+
+class LogsViewController:UITableViewController{
+
+private struct Storyboard{
+    static let showQuoteSegueIdentifier = "ShowQuote"
+    static let topicCellIdentifier = "TopicCell"
+}
+
+//Helpers for dates
+
+/*let dateFormatter = DateFormatter()
+
+dateFormatter.dateFormat = "dd/MM/yyyy"*/
+
+    //Table view data source
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.topicCellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = "\(LogsDeck.sharedInstance.logs[indexPath.row].date) \t\t\t Pushups: \(LogsDeck.sharedInstance.logs[indexPath.row].pushups)"
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return LogsDeck.sharedInstance.logs.count
+    }
+    
+    //Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        //selectedTopic = LogsDeck.sharedInstance.logs[indexPath.row]
+        performSegue(withIdentifier: Storyboard.showQuoteSegueIdentifier, sender: self)
+    }
+
+    
+}
+
---
+++ Project 1 Mahe Nick/Models/Log.swift
@@ -0,0 +1,22 @@
+//
+//  Log.swift
+//  Project 1 Mahe Nick
+//
+//  Created by Nick Mahe on 10/2/19.
+//  Copyright © 2019 Nick Mahe. All rights reserved.
+//
+
+import Foundation
+
+class Log {
+    //Properties
+    
+    var date: String
+    var pushups: Int
+
+    
+    init(date: String, pushups: Int){
+        self.date = date
+        self.pushups = pushups
+    }
+}
